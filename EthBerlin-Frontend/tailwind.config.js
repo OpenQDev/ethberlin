0fd1a4ad1f2190671829b0e4d73453eeeef278b4
ArcAnya <arcanyatwitch@gmail.com>
2022-09-17 20:37:43 +0200

    small design changes connect modal

--- EthBerlin-Frontend/tailwind.config.js
+++ EthBerlin-Frontend/tailwind.config.js
@@ -1,10 +1,16 @@
-module.exports = {  
-  content: [   
-        "./pages/**/*.{js,ts,jsx,tsx}",   
-        "./components/**/*.{js,ts,jsx,tsx}",  
-         ],
-  theme: {   
-         extend: {},
-         },  
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "dark-mode": "#0D1117",
+        "overlay": "#000000dd",
+        "muted": "rgb(139, 148, 158)",
+      },
+    },
+  },
   plugins: [],
-}
+};
