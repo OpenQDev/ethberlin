7e161c360a7ea83b01937c23b79ea1a8ed1b0a12
FlacoJones <andrew@openq.dev>
2022-09-17 15:46:20 +0200

    connects web3

--- EthBerlin-Frontend/pages/_app.js
+++ EthBerlin-Frontend/pages/_app.js
@@ -1,10 +1,20 @@
 import '../styles/globals.css';
 import StoreProvider from '../store/Store/StoreProvider';
+import { Web3ReactProvider } from '@web3-react/core';
+import { ethers } from 'ethers';
 
 function MyApp({ Component }) {
+  function getLibrary(provider) {
+    const library = new ethers.providers.Web3Provider(provider);
+    library.pollingInterval = 12000;
+    return library;
+  }
+
   return (
     <StoreProvider>
-      <Component />
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <Component />
+      </Web3ReactProvider>
     </StoreProvider>
   );
 };
