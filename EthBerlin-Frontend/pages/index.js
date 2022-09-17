ba653eedac4c2d29713fae55aa1efde30a9c04f7
FlacoJones <andrew@openq.dev>
2022-09-17 14:16:35 +0200

    beginning to add WalletConnect

--- EthBerlin-Frontend/pages/index.js
+++ EthBerlin-Frontend/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import AuthButton from '../components/Authentication/AuthButton';
+import ConnectButton from '../components/WalletConnect/ConnectButton';
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <AuthButton redirectUrl="http://localhost:3000" propicUrl="https://avatars.githubusercontent.com/u/93455288?v=4" />
+        <ConnectButton />
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
