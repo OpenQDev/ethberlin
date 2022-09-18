3581ef6962a25c721e84cb84e2e6d45589bb9851
rick <l.riccardo@web.de>
2022-09-18 10:01:19 +0200

    add UI following module

--- EthBerlin-Frontend/pages/index.js
+++ EthBerlin-Frontend/pages/index.js
@@ -32,7 +32,9 @@ export default function Home() {
  */
   return (
     <div>
-      <main className="flex justify-center">
+      <main>
+        <div className="flex flex-row space-x-5">
+        <div className="px-10 h-max font-semibold pt-5">Following</div>
         <div className="flex flex-wrap justify-center p-4 max-w-[1200px]">
           {localVids ?
             (
@@ -58,6 +60,7 @@ export default function Home() {
               }
               )
             )}
+        </div>
         </div>
       </main>
     </div>
