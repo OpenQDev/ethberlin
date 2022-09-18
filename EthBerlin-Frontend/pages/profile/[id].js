b7ae2477f6c6f7e0e7917513d1f628250e979d4f
ArcAnya <arcanyatwitch@gmail.com>
2022-09-18 10:19:22 +0200

    basic profile design

--- EthBerlin-Frontend/pages/profile/[id].js
+++ EthBerlin-Frontend/pages/profile/[id].js
@@ -1,10 +1,10 @@
-import { useRouter } from 'next/router';
-import { useState, useEffect, useContext } from 'react';
-import { client, getProfiles, getWalletProfile } from '../../api';
-import { ethers } from 'ethers';
-import useWeb3 from '../../hooks/useWeb3';
+import { useRouter } from "next/router";
+import { useState, useEffect, useContext } from "react";
+import { client, getProfiles, getWalletProfile } from "../../api";
+import { ethers } from "ethers";
+import useWeb3 from "../../hooks/useWeb3";
 import StoreContext from "../../store/Store/StoreContext";
-import VideoMini from '../../components/VideoMini/VideoMini';
+import VideoMini from "../../components/VideoMini/VideoMini";
 
 export default function Profile() {
   const router = useRouter();
@@ -23,7 +23,6 @@ export default function Profile() {
     }
   }, [id]);
 
-
   async function fetchProfile() {
     try {
       const response = await client.query(getProfiles, { id }).toPromise();
@@ -35,12 +34,14 @@ export default function Profile() {
 
   async function fetchLensHandle() {
     const accounts = await window.ethereum.request({
-      method: "eth_requestAccounts"
+      method: "eth_requestAccounts",
     });
     const address = accounts[0];
     console.log("waller found: ", address);
     try {
-      const response = await client.query(getWalletProfile, { address }).toPromise();
+      const response = await client
+        .query(getWalletProfile, { address })
+        .toPromise();
       setVerifiedLensProfile(response.data.profiles.items[0].name);
     } catch (err) {
       console.log(err);
@@ -50,47 +51,65 @@ export default function Profile() {
   async function followUser() {
     console.log(library);
     try {
-      const response = await appState.lensClient.followUser(library, [id], [0x0]);
+      const response = await appState.lensClient.followUser(
+        library,
+        [id],
+        [0x0]
+      );
       console.log(`response from lens client ... ${response}`);
     } catch (err) {
-      console.log('error: ', err);
+      console.log("error: ", err);
     }
   }
 
   useEffect(() => {
     async function foo() {
-      const response = await appState.pinataService.fetchVideoForUser('flacojones.lens');
+      const response = await appState.pinataService.fetchVideoForUser(
+        "flacojones.lens"
+      );
       setVideos(response);
     }
 
     foo();
   }, []);
 
-
   if (!profile) return null;
 
   return (
-    <div className="pt-3 pl-5">
-      <div className="pt-10 pl-5 text-2xl font-bold">Profile</div>
-      <div className="pt-5 pl-5">{id}</div>
-      <div className="pt-2 pl-5">{profile.handle}</div>
-      <div className="pt-5 pl-5">{profile.bio}</div>
-      <div className="pt-5 pl-5">Followers: {profile.stats.totalFollowers}</div>
-      <div className="pt-5 pl-5">Following: {profile.stats.totalFollowing}</div>
-      <div className="pt-3">
-        <button onClick={followUser} className="bg-blue-500 hover:bg-blue-300 text-white font-bold py-2 px-4 rounded">Follow User</button>
+    <div className="flex flex-col justify-center items-center p-8 w-full">
+      <div className="flex flex-col bg-nav-bg self-center items-center space-y-4 border border-gray-700 mb-4 p-8 px-24">
+        <div className=" text-2xl font-bold">Profile</div>
+        <div>
+          <div className="">User id: {id}</div>
+          <div className="">Handle: {profile.handle}</div>
+          <div className="">Bio: {profile.bio}</div>
+          <div className="">Followers: {profile.stats.totalFollowers}</div>
+          <div className="">Following: {profile.stats.totalFollowing}</div>
+        </div>
+        <div className="pt-3">
+          <button
+            onClick={followUser}
+            className="btn-primary font-bold py-2 px-4"
+          >
+            Follow User
+          </button>
+        </div>
       </div>
       {videos.map((foo, i) => {
         return (
           <div key={foo.metadata.keyvalues.pullRequestId} className="flex p-4">
-            <button onClick={() => router.push(`/pullRequest/${foo.metadata.keyvalues.pullRequestId}`)}>
+            <button
+              onClick={() =>
+                router.push(
+                  `/pullRequest/${foo.metadata.keyvalues.pullRequestId}`
+                )
+              }
+            >
               <VideoMini cid={foo.ipfs_pin_hash} />
             </button>
           </div>
         );
-      }
-      )
-      }
+      })}
     </div>
   );
 }
