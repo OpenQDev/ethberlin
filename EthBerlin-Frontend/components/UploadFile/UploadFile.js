7b18e1bf15ddd26e93eecceb776e8088d6ba0dbb
ArcAnya <arcanyatwitch@gmail.com>
2022-09-18 09:36:42 +0200

    re add design Upload

--- EthBerlin-Frontend/components/UploadFile/UploadFile.js
+++ EthBerlin-Frontend/components/UploadFile/UploadFile.js
@@ -1,12 +1,12 @@
 import { useState, useContext, useEffect } from "react";
 import StoreContext from "../../store/Store/StoreContext";
 
-// Curtesy from Pinata https://gist.github.com/stevedsimkins/6ac80b5eb9736fb29d9056f4440e71f1 
+// Curtesy from Pinata https://gist.github.com/stevedsimkins/6ac80b5eb9736fb29d9056f4440e71f1
 // Thanks Steve!
 
-const UploadFile = ({ pullRequestId, lensHandle}) => {
-  console.log('pullRequestId', pullRequestId);
-  console.log('lensHandle', lensHandle);
+const UploadFile = ({ pullRequestId, lensHandle }) => {
+  console.log("pullRequestId", pullRequestId);
+  console.log("lensHandle", lensHandle);
 
   const [selectedFile, setSelectedFile] = useState();
   const [appState, dispatch] = useContext(StoreContext);
@@ -30,50 +30,45 @@ const UploadFile = ({ pullRequestId, lensHandle}) => {
     setSelectedFile(event.target.files[0]);
   };
 
-
   const handleSubmission = async () => {
-
     const formData = new FormData();
 
-    formData.append('file', selectedFile);
+    formData.append("file", selectedFile);
 
     const metadata = JSON.stringify({
-      name: 'File name',
+      name: "File name",
       keyvalues: {
         pullRequestId,
-        lensHandle
-      }
+        lensHandle,
+      },
     });
 
-    formData.append('pinataMetadata', metadata);
+    formData.append("pinataMetadata", metadata);
 
     try {
       const IpfsHash = await appState.pinataService.uploadVideo(formData);
       console.log("ipfs hash here: ", IpfsHash);
 
-      if(window.localStorage.getItem('videos')) {
-        let videos = JSON.parse(window.localStorage.getItem('videos'));
+      if (window.localStorage.getItem("videos")) {
+        let videos = JSON.parse(window.localStorage.getItem("videos"));
         console.log("video upload array", videos);
-        videos.push([IpfsHash, pullRequestId])
-        window.localStorage.setItem('videos', JSON.stringify(videos))
+        videos.push([IpfsHash, pullRequestId]);
+        window.localStorage.setItem("videos", JSON.stringify(videos));
       } else {
-      const arr = [
-        [IpfsHash , pullRequestId],
-      ];
-      window.localStorage.setItem('videos', JSON.stringify(arr));
+        const arr = [[IpfsHash, pullRequestId]];
+        window.localStorage.setItem("videos", JSON.stringify(arr));
       }
-
     } catch (error) {
       console.log(error);
     }
   };
 
   return (
-    <>
-      <label className="form-label">Choose File</label>
-      <input type="file" onChange={changeHandler} />
-      <button onClick={handleSubmission}>Submit</button>
-    </>
+    <div className="flex flex-col gap-8 justify-center items-center bg-nav-bg border border-gray-700 w-1/2 h-1/2">
+      <label className="form-label text-lg">Choose File</label>
+      <input className="input-field btn-default flex p-2 border" type="file" onChange={changeHandler} />
+      <button className="btn-primary px-12 py-1" onClick={handleSubmission}>Submit</button>
+    </div>
   );
 };
 
